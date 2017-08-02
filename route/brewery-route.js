87ce7f58103ce2c1923bb122b6c9ec9977975e0c
Jamesbillard12 <jamesbillard12@hotmail.com>
2017-08-02 14:55:47 -0700

    finished route and server

---
+++ route/brewery-route.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const Router = require('express').Router;
+const jsonParser = require('body-parser').json();
+const debug = require('debug')('brew:brewery-router');
+const Brewery = require('../model/brewery.js');
+const breweryRouter = module.exports = new Router();
+
+breweryRouter.post('/api/brewery', jsonParser, function (req, res, next) {
+  debug('POST: /api/brewery');
+
+  req.body.timestamp = new Date();
+  new Brewery(req.body).save()
+  .then( brewery => res.json(brewery))
+  .catch(next);
+});
+
+breweryRouter.get('/api/brewery/:id', function (req, res, next) {
+  debug('GET: /api/brewery/:id');
+
+  Brewery.findById(req.param.id)
+  .then( brewery => res.json(brewery))
+  .catch(next);
+});
+
+breweryRouter.put('/api/brewery/:id', function (req, res, next) {
+  debug('GET: /api/brewery/:id');
+
+  Brewery.findByIdAndUpdate(req.param.id, req.body)
+  .then( brewery => res.json(brewery))
+  .catch(next);
+});
+
+breweryRouter.delete('/api/brewery/:id', function (req, res, next) {
+  debug('GET: /api/brewery/:id');
+
+  Brewery.findByIdAndRemove(req.param.id)
+  .catch(next);
+});
--- server.js
+++ server.js
@@ -4,10 +4,20 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const Promise = require('bluebird');
+const mongoose = require('mongoose');
 const debug = require('debug')('bew:server');
+const breweryRouter = require('./route/brewery-router.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGODB_URI = 'mongodp://localhost/brewerybeers';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(breweryRouter);
 
 app.listen(PORT, () => {
   debug(`listening on ${PORT}`);
