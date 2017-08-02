5621ca3b90192e295a9f4c507230df163ce95a5e
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-08-02 15:54:36 -0700

    Added middleware to server.js, connected to MongoDB database

--- lab-eddie/server.js
+++ lab-eddie/server.js
@@ -4,14 +4,22 @@ const express = require('express');
 const morgan = require('morgan');
 const debug = require('debug')('app:server');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const Promise = require('bluebird');
+const bandRouter = require('./routes/band-router.js');
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGODB_URI = 'mongodb://localhost/ledzeppelin';
 
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
-
+app.use(cors);
+app.use(debug('dev'));
+app.use(bandRouter);
 
 app.listen(PORT, () => {
   debug('Server active on port: ', PORT);
-})
+});
