053065b0c8acb1197a7247d6113a5cabfb42238a
Luis Manzo <manzosk3@gmail.com>
2017-08-02 15:02:51 -0700

    defines schema and exports model, server saves a document to Airports collection

---
+++ model/airport.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//TODO -- require later
+const airportSchema = new Schema({
+  name : { type: String, require: true },
+  iata : { type: String, require: false },
+  city : { type: String, require: false }
+});
+
+
+module.exports = mongoose.model('Airport', airportSchema);
--- server.js
+++ server.js
@@ -6,9 +6,17 @@ const debug = require('debug')('airport:server.js');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
 const MONGODB_URL = 'mongodb://localhost/airports_401';
+const Airport = require('./model/airport.js');
 
 app.use(cors());
 app.use(morgan('dev'));
+
+mongoose.connect(MONGODB_URL);
+const lax = new Airport({ name : 'test'});
+lax.save(function(err) {
+  if(err) return err;
+  console.log('new airport saved');
+})
 
 app.listen(PORT, function() {
   debug(`listening on port ${PORT}`);
