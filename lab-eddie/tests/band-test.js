8317ac88dde3158459864ef322c34d4c6a541de3
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-08-02 22:48:31 -0700

    Wrote test for delete

--- lab-eddie/tests/band-test.js
+++ lab-eddie/tests/band-test.js
@@ -5,10 +5,10 @@ const expect = require('chai').expect;
 const request = require('superagent');
 
 const Band = require('../model/band.js');
-const url = 'http://localhost:8000';
+const url = 'http://localhost:3000';
 
 let testingBand = {
-  name: 'Led Zeppeling',
+  name: 'Led Zeppelin',
   genre: 'Rock',
   hometown: 'London?'
 }
@@ -24,16 +24,42 @@ describe('Band Test stuff', function() {
     });
 
     it('res should be a band.', done => {
-      request.post('localhost:8000/api/band')
-        .send(testingBand)
-        .end((err, res) => {
-          if(err) throw new Error(err);
-          expect(res.status).to.equal(200);
-          expect(res.body.name).to.equal(testingBand.name);
-          expect(res.body.genre).to.equal(testingBand.genre);
-          expect(res.body.hometown).to.equal(testingBand.hometown);
-          done();
-        });
+      request.post('localhost:3000/api/band')
+      .send(testingBand)
+      .end((err, res) => {
+        if(err) throw new Error(err);
+        expect(res.status).to.equal(200);
+        expect(res.body.name).to.equal(testingBand.name);
+        expect(res.body.genre).to.equal(testingBand.genre);
+        expect(res.body.hometown).to.equal(testingBand.hometown);
+        done();
+      });
     });
-  });  
+  }); 
+  describe('GET: /api/band', function() {
+    before(done => {
+      Band.create(testingBand)
+      .then(band => {
+        this.tempBand = band;
+        done();
+      })
+      .catch(err => done(err));
+    });
+    after(done => {
+      Band.remove({})
+      .then(() => done())
+      .catch(err => done(err));
+    });
+    it('should get a band back.', done => {
+      request.get(`localhost:3000/api/band/${this.tempBand._id}`, (err, res) => {
+        if(err) throw new Error(err);
+        expect(res.status).to.equal(200);
+        expect(res.body._id).to.be.equal(this.tempBand._id.toString());
+        expect(res.body.name).to.equal(this.tempBand.name);
+        expect(res.body.genre).to.equal(this.tempBand.genre);
+        expect(res.body.hometown).to.equal(this.tempBand.hometown);
+        done();
+      });
+    });
+  });
 });
