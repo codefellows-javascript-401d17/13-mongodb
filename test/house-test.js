807a32a65e89fafd0d7a3944ba8b0ac2653f8f67
Allan Liebold <allan.liebold@gmail.com>
2017-08-12 18:05:53 -0700

    adds put test and 404 for get test

--- test/house-test.js
+++ test/house-test.js
@@ -3,40 +3,51 @@
 const expect = require('chai').expect;
 const request = require('superagent');
 const House = require('../model/house.js');
-const PORT = process.env.PORT || 3000;
-const mongoose = require('mongoose');
 
-mongoose.Promise = Promise;
 require('../server.js');
+const PORT = process.env.PORT || 3000;
+
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
 
 const url = `http://localhost:${PORT}`;
+
 const exampleHouse = {
   name: 'Atreides',
   seat: 'Caladan',
   region: 'Delta Pavonis',
   words: 'Fear is the Mind-Killer'
-}
+};
+
+const updateHouse = {
+  seat: 'Arrakeen',
+  region: 'Arrakis',
+};
 
 describe('House Routes', function() {
   describe('POST: /api/house', function() {
-    describe('with a valid request body', function() {
-      after( done => {
+    describe('with a valid req body', function() {
+      after(done => {
         if(this.tempHouse) {
           House.remove({})
-          .then( () => done())
+          .then(() => done())
           .catch(done);
           return;
         }
         done();
       });
+
       it('should return a house', done => {
         request.post(`${url}/api/house`)
         .send(exampleHouse)
-        .end((err, response) => {
+        .end((err, res) => {
           if(err) return done(err);
-          expect(response.status).to.equal(200);
-          expect(response.body.name).to.equal('Atreides');
-          this.tempHouse = response.body;
+          expect(res.status).to.equal(200);
+          expect(res.body.name).to.equal('Atreides');
+          expect(res.body.seat).to.equal('Caladan');
+          expect(res.body.region).to.equal('Delta Pavonis');
+          expect(res.body.words).to.equal('Fear is the Mind-Killer');
+          this.tempHouse = res.body;
           done();
         });
       });
@@ -45,21 +56,21 @@ describe('House Routes', function() {
 
   describe('GET: /api/house/:id', function() {
     describe('with a valid body', function() {
-      before( done => {
+      before(done => {
         exampleHouse.timestamp = new Date();
         new House(exampleHouse).save()
-        .then( house => {
+        .then(house => {
           this.tempHouse = house;
           done();
         })
         .catch(done);
       });
 
-      after( done => {
+      after(done => {
         delete exampleHouse.timestamp;
         if(this.tempHouse) {
           House.remove({})
-          .then( () => done())
+          .then(() => done())
           .catch(done);
           return;
         }
@@ -72,8 +83,50 @@ describe('House Routes', function() {
           if(err) return done(err);
           expect(res.status).to.equal(200);
           expect(res.body.name).to.equal('Atreides');
+          expect(res.body.seat).to.equal('Caladan');
+          expect(res.body.region).to.equal('Delta Pavonis');
+          expect(res.body.words).to.equal('Fear is the Mind-Killer');
           done();
         });
+      });
+
+      it('should return status 404', done => {
+        request.get(`${url}/api/house/1234`)
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('PUT: /api/house/:id', function() {
+    before(done => {
+      exampleHouse.timestamp = new Date();
+      new House(exampleHouse).save()
+      .then(house => {
+        this.testHouse = house;
+        done();
+      })
+      .catch(done);
+    });
+
+    after(done => {
+      House.remove({})
+      .then(() => done())
+      .catch(done);
+    });
+
+    it('should return updated house', done => {
+      console.log('testHouse id:', this.testHouse._id);
+      request.put(`${url}/api/house/${this.testHouse._id}`)
+      .send(updateHouse)
+      .end((err, res) => {
+        if(err) return done(err);
+        expect(res.status).to.equal(200);
+        expect(res.body.seat).to.equal('Arrakeen');
+        expect(res.body.region).to.equal('Arrakis');
+        done();
       });
     });
   });
